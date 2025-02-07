4ee0fc0a1fad643ca3bd9940e09244aafc156229
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-02-07 07:19:10 +0600

    require the daysi ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daysiui from "daisyui";
 
 export default {
   content: [
@@ -14,5 +15,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    daysiui,
+  ],
 } satisfies Config;
