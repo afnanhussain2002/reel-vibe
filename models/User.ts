90043dbb18099b5a230a6bbd1314bd44819ffe70
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-02-10 14:45:21 +0600

    save the user

--- models/User.ts
+++ models/User.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { model, models, Schema } from "mongoose";
 import bcrypt from "bcryptjs";
 
 
@@ -16,9 +16,14 @@ const userSchema = new Schema<IUser>(
         password: { type: String, required: true },
     },{timestamps:true}
 )
+
 userSchema.pre("save", async function (next) {
     if (this.isModified("password")) {
        this.password = await bcrypt.hash(this.password, 10);
     }
     next();
 })
+
+const User = models?.User || model<IUser>("User", userSchema);
+export default User
+
