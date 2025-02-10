483302d4f33414025acf932fafb8a6720904c017
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-02-10 14:35:43 +0600

    define the type of user

--- models/User.ts
+++ models/User.ts
@@ -1,2 +1,10 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+
+export interface IUser {
+  email: string;
+  password: string;
+  _id?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
