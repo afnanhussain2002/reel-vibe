22f7f8a4e30cc5d83da9bd0ee72d3b53ff1b3326
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-02-11 18:23:34 +0600

    write the video schema code

--- models/Video.ts
+++ models/Video.ts
@@ -20,3 +20,19 @@ export const VIDEO_DIMENSIONS = {
     createdAt?: Date;
     updatedAt?: Date;
   }
+
+  const videoSchema = new Schema<IVideo>(
+    {
+      title: { type: String, required: true },
+      description: { type: String, required: true },
+      videoUrl: { type: String, required: true },
+      thumbnailUrl: { type: String, required: true },
+      controls: { type: Boolean, default: true },
+      transformation: {
+        height: { type: Number, default: VIDEO_DIMENSIONS.height },
+        width: { type: Number, default: VIDEO_DIMENSIONS.width },
+        quality: { type: Number, min: 1, max: 100 },
+      },
+    },
+    { timestamps: true }
+  );
