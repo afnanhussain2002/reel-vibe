ef128b588153458c6f0bfd2399e8e96aa2fd596d
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-02-15 14:41:06 +0600

    write the credentials

--- lib/option.ts
+++ lib/option.ts
@@ -2,6 +2,13 @@ import { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
    providers: [
-       
+       CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+            email: { label: "Email", type: "email", },
+            password: { label: "Password", type: "password" }
+        },
+        
+       })
    ]
 }
