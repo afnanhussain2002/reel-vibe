282ae5f986c06580e1b72625e40d8d3ad94b68aa
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-02 11:53:10 +0600

    return true if found that path

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,17 @@ export default withAuth(
     },
     {
       callbacks:{
-        
+        authorized: ({token, req}) => {
+          const {pathname} = req.nextUrl;
+
+          if (
+            pathname.startsWith("/api/auth") ||
+            pathname === "/login" ||
+            pathname === "/register"
+          ) {
+            return true
+          }
+        }
       }
     }
 )
