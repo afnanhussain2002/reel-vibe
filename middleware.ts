1d39f05e2e986696b5b155b6b1c0432289968527
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-02 11:43:11 +0600

    start write the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,2 +1,8 @@
 import withAuth from "next-auth/middleware";
 import { NextResponse } from "next/server";
+
+export default withAuth(
+    function middleware() {
+       return NextResponse.next()
+    }
+)
