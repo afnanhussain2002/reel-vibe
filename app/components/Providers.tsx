8d12d2844940974fe7c634711c3caf67fe3cdad7
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-04 23:49:18 +0600

    change the position of authenticator

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -5,25 +5,26 @@ import { SessionProvider } from "next-auth/react";
 const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
 
-const authenticator = async () => {
-    try {
-      const response = await fetch("/api/imagekit-auth");
-  
-      if (!response.ok) {
-        const errorText = await response.text();
-        throw new Error(`Request failed with status ${response.status}: ${errorText}`);
-      }
-  
-      const data = await response.json();
-      const { signature, expire, token } = data;
-      return { signature, expire, token };
-    } catch (error) {
-      console.error("ImageKit authentication error:", error);
-      throw new Error(`Authentication request failed: ${error}`);
-    }
-  };
+
   
   export default function Providers({children}:{children: React.ReactNode}) {
+    const authenticator = async () => {
+        try {
+          const response = await fetch("/api/imagekit-auth");
+      
+          if (!response.ok) {
+            const errorText = await response.text();
+            throw new Error(`Request failed with status ${response.status}: ${errorText}`);
+          }
+      
+          const data = await response.json();
+          const { signature, expire, token } = data;
+          return { signature, expire, token };
+        } catch (error) {
+          console.error("ImageKit authentication error:", error);
+          throw new Error(`Authentication request failed: ${error}`);
+        }
+      };
     return (
    <SessionProvider>
        <ImageKitProvider urlEndpoint={urlEndpoint} publicKey={publicKey} authenticator={authenticator}>
