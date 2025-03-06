8ce653b25190020c0d43058f5c0ad5e4ab72f71b
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-06 23:41:41 +0600

    write the post request for post a video

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -17,3 +17,7 @@ export async function GET(request: Request) {
         return NextResponse.json({error: error}, {status: 500});
       }
 }
+
+export async function POST(request: Request) {
+    
+}
