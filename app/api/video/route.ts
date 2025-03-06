b12b7772ed4d02b3c63dbfc201bda27573528844
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-06 23:44:32 +0600

    return a response if no user found

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -22,4 +22,8 @@ export async function GET() {
 
 export async function POST(request: NextRequest) {
     const session = await getServerSession(authOptions);
+
+    if (!session) {
+        return NextResponse.json({error: "Unauthorized"}, {status: 401});
+    }
 }
