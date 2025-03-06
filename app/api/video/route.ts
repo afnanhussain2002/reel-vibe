ba8a6bf09c7168fce39659b1360ee0aac22cdf28
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-06 23:34:55 +0600

    handle the catch part

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,9 +1,10 @@
+import { NextResponse } from "next/server";
 
 
 export async function GET(request: Request) {
       try {
         
       } catch (error) {
-        
+        return NextResponse.json({error: error}, {status: 500});
       }
 }
