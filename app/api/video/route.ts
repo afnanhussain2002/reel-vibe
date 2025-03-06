4b75f8f00f836d631e9b39a680e56bbace08b720
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-06 23:40:50 +0600

    return all video if video is found

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -10,7 +10,9 @@ export async function GET(request: Request) {
         if (!videos || videos.length === 0) {
             return NextResponse.json({error: "No videos found"}, {status: 404});
         }
-        
+
+        return NextResponse.json(videos, {status: 200});
+
       } catch (error) {
         return NextResponse.json({error: error}, {status: 500});
       }
