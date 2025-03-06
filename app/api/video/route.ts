516d72c40eda9ec4838a3c2c3a1b66a05a7753b0
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-06 23:35:36 +0600

    connect to db

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,9 +1,10 @@
+import { dbConnect } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 
 export async function GET(request: Request) {
       try {
-        
+        await dbConnect();
       } catch (error) {
         return NextResponse.json({error: error}, {status: 500});
       }
