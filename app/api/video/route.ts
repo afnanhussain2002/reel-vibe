77964cab467a09956826716bdda108d76b8abc16
afnanhussain2002 <afnanhussain2022@gmail.com>
2025-03-06 23:36:08 +0600

    find all videos on db

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,10 +1,12 @@
 import { dbConnect } from "@/lib/db";
+import Video from "@/models/Video";
 import { NextResponse } from "next/server";
 
 
 export async function GET(request: Request) {
       try {
         await dbConnect();
+        const videos = await Video.find();
       } catch (error) {
         return NextResponse.json({error: error}, {status: 500});
       }
